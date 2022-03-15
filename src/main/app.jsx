f08bca3b38736fc8ebee630c8cf888b99721072b
marcelo-betassa <3986286+marcelob-tech@users.noreply.github.com>
2022-03-15 00:05:43 -0300

    commit inicial do front todo-app

---
+++ src/main/app.jsx
@@ -0,0 +1,18 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
+
+
+const App = (props) => {
+  return (
+    <div className='container'>
+        <Todo/>
+        <About/>
+    </div>
+  )
+}
+
+export default App;
